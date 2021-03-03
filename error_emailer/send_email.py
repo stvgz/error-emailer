c012cb0c5745f0417da632224faaee17b7a69b29
SteveGaoZhitao <zhitao.gao@outlook.com>
2021-03-03 21:47:03 +0800

    add test cases

---
+++ error_emailer/send_email.py
@@ -0,0 +1,117 @@
+# Send email through simple smtp
+
+from email.mime.multipart import MIMEMultipart
+import sys
+import traceback
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr,parseaddr
+
+from typing import List
+
+class EmailError():
+
+    def __init__(self, from_addr: str = None, to_addr : str or List = None, smtp_addr : str = None, 
+                error_msg_title = 'Error', 
+                error_msg_content = 'type+value+traceback',
+                raise_on_error = False, print_on_error = False, send_email = True, 
+                verbose = False ) -> None:
+        """
+        Email error message, tracebacks when error occurs
+        Email sent through simple smtp withou any authentication process. 
+        Suits most of internal smtp service
+
+        Input
+        ----- 
+        from_addr: str represents email address
+        to_addr: str represnets email address or list of address
+
+        """
+
+        self.from_addr = from_addr
+
+        if type(to_addr) is str:
+            self.to_addr = [to_addr]
+        else:
+            self.to_addr = to_addr
+
+        self.smtp_addr = smtp_addr
+        self.error_msg_title = error_msg_title
+        self.error_msg_content = error_msg_content
+        self.raise_on_error = raise_on_error,
+        self.print_on_error = print_on_error,
+        self.send_email = send_email
+        self.verbose = verbose
+
+        self.msg = None
+        
+    
+    def _format_addr(self, s):
+        name, addr = parseaddr(s)
+        return formataddr((Header(name, 'utf-8').encode(), addr))
+
+
+    def make_email(self):
+        
+        msg = MIMEMultipart('mixed')
+
+        msg['From'] = self._format_addr("From <{}>".format(self.from_addr))
+        msg['To'] = self._format_addr("To <{}>".format(self.from_addr))
+        msg['Subject'] = Header(self.error_msg_title, 'utf-8').encode()
+
+        msg.attach(MIMEText(self.error_msg_content, 'plain', 'utf-8'))
+
+        self.msg = msg
+
+
+    def send_email(self):
+        
+        assert self.smtp_addr is not None, "Please Privide SMTP Addr"
+        
+        s = smtplib.SMTP(self.smtp_addr)
+        s.sendmail(self.from_addr, self.to_addr, self.msg.as_string())
+        s.quit()
+
+        print('Sent Email from {} to {} address. '.format(self.from_addr, len(self.to_addr)))
+
+    def email_on_error(self):
+
+        def wrapper(func):
+
+            def deco(*args, **kwargs):
+                
+                try:
+
+                    func(*args,**kwargs)
+
+                except:
+
+                    type, value, traceback = sys.exc_info()
+
+                    if self.print_on_error:
+                        print("Catched Error During executing", type, value, traceback)
+                    
+                    if self.send_email:
+                        
+
+                        self.make_email()
+
+                        self.send_email()
+
+                        if not self.verbose:
+                            
+                            print('Sent email')
+                    else:
+                        if not self.verbose:
+                            print('Skipped emailing. send email set to False')
+                        
+
+                    
+
+                
+            return deco
+        
+        return wrapper
+
---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+from error_emailer import EmailError
+class TestEmail(unittest.TestCase):
+
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+
+    def test_email(self):
+
+        ee = EmailError(from_addr='me', 
+                        to_addr = 'others@example.com',
+                        smtp_addr = '1.2.3.4',
+                        email_on_error = False)
+
+        @ee.email_on_error()
+        def make_a_error():
+
+            a = 1
+            b = '2'
+            print(a + b)
+
+            return a
+
+
+        r = make_a_error()
+
+        self.assertEqual(1,1)
+
+
+if __name__ == '__main__':
+    unittest.main()
